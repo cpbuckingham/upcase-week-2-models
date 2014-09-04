1d10900e9a103ce2647eeb2fc280519ccfae691f
Cameron <cameron.p.buckingham@gmail.com>
2014-09-03 21:19:07 -0600

    Week 2: Rails Models

---
+++ upcase-week-2-models/app/controllers/decks_controller.rb
@@ -0,0 +1,30 @@
+class DecksController < ApplicationController
+
+  def index
+    @decks = Deck.all
+  end
+  def show
+    @deck = Deck.find(params[:id])
+  end
+  def new
+    @deck = Deck.new
+  end
+  def create
+    @deck = Deck.new(name: params[:deck][:name])
+    @deck.save
+    redirect_to "/decks"
+  end
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+  def update
+    @deck = Deck.find(params[:id])
+    @deck.update(name: params[:deck][:name])
+    redirect_to "/decks"
+  end
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy
+    redirect_to "/decks"
+  end
+end
